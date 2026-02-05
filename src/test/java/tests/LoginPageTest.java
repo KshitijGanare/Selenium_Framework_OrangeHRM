07e895ff27478622a11ec4ae4c2b78986e8f26b8
Kshitij Raju Ganare <kshitij.ganare@truworthwellness.com>
2026-02-05 15:41:13 +0530

    testcases login and usermanagement

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,4 +1,44 @@
 package tests;
 
-public class LoginPageTest {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+import java.io.IOException;
+
+public class LoginPageTest extends BaseTest {
+
+    LoginPage login;
+
+    @BeforeMethod
+    public void setupPage() {
+        login = new LoginPage();
+    }
+
+    @Test
+    public void verifyLoginWithValidCredentialsTest() throws IOException {
+        String expUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+        String actUrl = login.verifyLoginWithValidCredentials();
+        Assert.assertEquals(actUrl, expUrl, "URL Mismatch");
+
+    }
+
+    @Test
+    public void verifyLoginWithInvalidCredentialsTest() throws IOException, InterruptedException {
+        String expMessage =  "Invalid credentials";
+        String actMessage = login.verifyLoginWithInvalidCredentials();
+        Assert.assertEquals(actMessage, expMessage, "Error Message not displayed");
+
+    }
+
+
+    @Test
+    public void verifyforgetPasswordWithValidUsername() throws IOException {
+        boolean expResult = true;
+        boolean actResult = login.verifyForgetPassword();
+        Assert.assertEquals(actResult, expResult, "Forget Password Failed!");
+    }
+
 }
--- src/test/java/tests/UserManagementTest.java
+++ src/test/java/tests/UserManagementTest.java
@@ -1,4 +1,35 @@
 package tests;
 
-public class UserManagementTest {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.UserManagement;
+
+import java.io.IOException;
+
+public class UserManagementTest extends BaseTest {
+
+    LoginPage login;
+    UserManagement user;
+
+    @BeforeMethod
+    public void setupPage() {
+        login = new LoginPage();
+        user = new UserManagement();
+
+    }
+
+
+    @Test
+    public void verifyAddUserTest() throws IOException, InterruptedException {
+       boolean expRes = true;
+       boolean actRes = user.verifyAddUser();
+
+       Assert.assertEquals(actRes, expRes);
+
+
+    }
+
 }
